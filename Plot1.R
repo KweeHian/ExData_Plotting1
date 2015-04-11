04c4b0ca1006783aade3bab28d47ca5d8e2e255f
KweeHian <ongkweehian@yahoo.com.sg>
2015-04-11 20:30:33 +0800

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,13 @@
+# Load Data
+ds <- read.csv("household_power_consumption.txt", header=T, na.string="?", sep=";")
+ds$Date <- as.Date(ds$Date, format="%d/%m/%Y")
+
+# Subset data and format Date
+subdata <- subset(ds, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+dtetme <- paste(as.Date(subdata$Date), subdata$Time)
+subdata$Datetime <- as.POSIXct(dtetme)
+
+# Save plot1 to png
+hist(subdata$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
